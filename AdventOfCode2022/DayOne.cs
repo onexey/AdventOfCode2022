ef4d92d73f8b4c960cc349840b736e262d4f7b5c
MS <onexey@gmail.com>
2022-12-13 20:53:31 +0300

    Day One

---
+++ AdventOfCode2022/DayOne.cs
@@ -0,0 +1,53 @@
+﻿namespace AdventOfCode2022;
+
+public class DayOne
+{
+    public void Run()
+    {
+        var input = LoadInput();
+
+        var elves = new Dictionary<int, int>();
+        var elfCounter = 0;
+
+        var numberOneElf = -1;
+        var numberOneElfFood = -1;
+
+        foreach (var s in input)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                elfCounter++;
+                continue;
+            }
+
+            elves.TryGetValue(elfCounter, out var currentTotal);
+            int.TryParse(s, out var lineVal);
+            currentTotal += lineVal;
+            elves[elfCounter] = currentTotal;
+
+            if (numberOneElfFood < currentTotal)
+            {
+                numberOneElf = elfCounter;
+                numberOneElfFood = currentTotal;
+            }
+        }
+
+        Console.WriteLine($"Number One Elf is: {numberOneElf} with {numberOneElfFood} calories");
+
+        var foodList = elves.Values.ToList();
+        foodList = foodList.OrderDescending().ToList();
+
+        var topThreeTotal = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            topThreeTotal += foodList[i];
+        }
+        
+        Console.WriteLine($"Top Three Total: {topThreeTotal} calories");
+    }
+
+    private string[] LoadInput()
+    {
+        return File.ReadAllLines(@"Inputs\DayOneInput.txt");
+    }
+}
---
+++ AdventOfCode2022/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using AdventOfCode2022;
+
+Console.WriteLine("Hello, World!");
+
+var dayOne = new DayOne();
+dayOne.Run();
